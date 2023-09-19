5aafb3fc1084b1f5e94b08f4594e4678ee43f7cd
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-19 17:32:57 +0200

    addin signature const

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include <stdio.h>
 
-void	*ft_memcpy(void *restrict dest, void *restrict src, size_t n)
+void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
 	size_t	i;
 
