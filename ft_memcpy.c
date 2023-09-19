8d6fe9aad20ba49042be3b72f456ce03e6165528
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-19 17:28:39 +0200

    refacto ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,31 +1,15 @@
 #include <stddef.h>
 #include <stdio.h>
 
-
 void	*ft_memcpy(void *restrict dest, void *restrict src, size_t n)
 {
-	size_t i;
-	char	*d;
-	char	*s;
+	size_t	i;
+
 	i = 0;
-	
-	d = dest;
-	s = src;
 	while (i < n)
 	{
-		d[i] = s[i];
+		((char *)dest)[i] = ((char *)src)[i];
 		i++;
 	}
-	return (d);
+	return (dest);
 }	
-
-#include <stdio.h>
-#include <string.h>
-int	main(void)
-{
-	char	dest[] = "bonjour";
-	char	src[] = "salut";
-	memcpy(dest, src, 3);
-	//ft_memcpy(dest, src, 2);
-	printf("%s", dest);
-}
