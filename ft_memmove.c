7e3d8f8ea3348f4c2bf1886e2a82b7401b599fca
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-20 09:58:56 +0200

    add function

---
+++ ft_memmove.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t len)
+{
+	char		*ptr_dest;
+	const char	*ptr_src;
+	size_t		i;
+
+	ptr_dest = dest;
+	ptr_src = src;
+	i = 0;
+	if (ptr_dest == ptr_src)
+		return (dest);
+	while (i < len)
+	{
+		ptr_dest[i] = ptr_src[i];
+		i++;
+	}
+	return (dest);
+}
