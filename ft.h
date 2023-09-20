13b1cc5263c91a1c683306c5c5b561ab67b6d273
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-20 10:00:14 +0200

    add signature ft_memmove

--- ft.h
+++ ft.h
@@ -5,6 +5,7 @@ unsigned int			ft_strlen(const char *str);
 void					*ft_memset(void *s, int c, unsigned int n);
 void					*ft_memcpy(void *restrict dest,
 							const void *restrict src, size_t n);
+void					*ft_memmove(void *dest, const void *src, size_t len);
 void					ft_bzero(void *s, size_t n);
 int						ft_isalpha(int c);
 int						ft_isdigit(int c);
