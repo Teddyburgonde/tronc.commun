e2779a7bb850b700ef18e51b64d23442d0c640fc
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-20 06:37:33 +0200

    modif pointeur

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,12 +3,16 @@
 
 void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
-	size_t	i;
+	size_t		i;
+	char		*ptr_dest;
+	const char	*ptr_src;
 
+	ptr_src = src;
+	ptr_dest = dest;
 	i = 0;
 	while (i < n)
 	{
-		((char *)dest)[i] = ((char *)src)[i];
+		ptr_dest[i] = ptr_src[i];
 		i++;
 	}
 	return (dest);
