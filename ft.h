335339b2fc2163352309142de4a9e3e0a74327b2
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-20 10:20:48 +0200

    add prototype ft_strlcpy

--- ft.h
+++ ft.h
@@ -3,8 +3,8 @@
 
 unsigned int			ft_strlen(const char *str);
 void					*ft_memset(void *s, int c, unsigned int n);
-void					*ft_memcpy(void *restrict dest,
-							const void *restrict src, size_t n);
+void					*ft_memcpy(void *dest, const void *src, size_t n);
+size_t					strlcpy(char *dst, const char *src, size_t dstsize);
 void					*ft_memmove(void *dest, const void *src, size_t len);
 void					ft_bzero(void *s, size_t n);
 int						ft_isalpha(int c);
