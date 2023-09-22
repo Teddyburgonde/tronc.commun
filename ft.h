27fdeba79864dcc3a69300449584c4fd079a36e1
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-22 09:55:18 +0200

    add proto ft_tolower

--- ft.h
+++ ft.h
@@ -5,7 +5,7 @@ unsigned int			ft_strlen(const char *str);
 void					*ft_memset(void *s, int c, unsigned int n);
 void					*ft_memcpy(void *dest, const void *src, size_t n);
 size_t					ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t					strlcat(char *dst, const char *src, size_t dstsize);
+size_t					ft_strlcat(char *dst, const char *src, size_t dstsize);
 void					*ft_memmove(void *dest, const void *src, size_t len);
 void					ft_bzero(void *s, size_t n);
 int						ft_isalpha(int c);
@@ -13,5 +13,6 @@ int						ft_isdigit(int c);
 int						ft_isalnum(int c);
 int						ft_isascii(int c);
 int						ft_isprint(int c);
-int						toupper(int c);
+int						ft_toupper(int c);
+int						ft_tolower(int c);
 #endif
