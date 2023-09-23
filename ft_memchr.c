b13408a6cb53dce57aa1449f810a679d8e6a3874
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-23 18:23:09 +0200

    -madd function ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const char	*tab;
+	size_t		i;
+
+	i = 0;
+	tab = s;
+	while (i < n)
+	{
+		if (tab[i] == c)
+			return ((void *)tab);
+		i++;
+	}
+	return (NULL);
+}
