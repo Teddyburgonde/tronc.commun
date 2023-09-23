f7211fb997562ec83db8d3b72986edaf60191043
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-23 08:43:40 +0200

    add proto ft_strrchr

--- ft.h
+++ ft.h
@@ -9,6 +9,7 @@ size_t					ft_strlcat(char *dst, const char *src, size_t dstsize);
 void					*ft_memmove(void *dest, const void *src, size_t len);
 void					ft_bzero(void *s, size_t n);
 char					*ft_strchr(const char *s, int c);
+char					*ft_strrchr(const char *s, int c);
 int						ft_isalpha(int c);
 int						ft_isdigit(int c);
 int						ft_isalnum(int c);
