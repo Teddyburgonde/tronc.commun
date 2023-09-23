9d99f1145851f2062172faf19eb49dc43024059e
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-23 09:57:50 +0200

    add proto ft_strncmp

--- ft.h
+++ ft.h
@@ -10,6 +10,7 @@ void					*ft_memmove(void *dest, const void *src, size_t len);
 void					ft_bzero(void *s, size_t n);
 char					*ft_strchr(const char *s, int c);
 char					*ft_strrchr(const char *s, int c);
+int						ft_strncmp(const char *s1, const char *s2, size_t n);
 int						ft_isalpha(int c);
 int						ft_isdigit(int c);
 int						ft_isalnum(int c);
