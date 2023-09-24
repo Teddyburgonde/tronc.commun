3c17c6da2b81682fad809dd272d298e0503e2113
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-24 09:01:50 +0200

    add proto

--- ft.h
+++ ft.h
@@ -8,6 +8,7 @@ size_t					ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t					ft_strlcat(char *dst, const char *src, size_t dstsize);
 void					*ft_memmove(void *dest, const void *src, size_t len);
 void					*ft_memchr(const void *s, int c, size_t n);
+int						ft_memcmp(const void *s1, const void *s2, size_t n);
 void					ft_bzero(void *s, size_t n);
 char					*ft_strchr(const char *s, int c);
 char					*ft_strrchr(const char *s, int c);
