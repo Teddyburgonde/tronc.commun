50392a5b8f1077dc90b84b6ed83f5593a332ac8e
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-24 19:36:59 +0200

    i must modif name array

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stddef.h>
 
 char	*ft_strnstr(const char *str, const char *to_find, int len)
 {
@@ -15,27 +15,11 @@ char	*ft_strnstr(const char *str, const char *to_find, int len)
 		{
 			if (i >= len)
 				break ;
-			if(to_find[j + 1] == '\0')
+			if (to_find[j + 1] == '\0')
 				return ((char *)&str[i]);
 			j++;
 		}
 		i++;
 	}
 	return (NULL);
-}
-
-
-#include <string.h>
-#include <stdio.h>
-
-int	main(int argc, char **argv)
-{
-	size_t max_len = strlen(argv[1]);
-	if (argc == 3)
-	{
-		printf("%s\n", ft_strnstr(argv[1], argv[2], max_len));
-		printf("%s", strnstr(argv[1], argv[2], max_len));
-		//printf("%s\n", ft_strnstr(argv[1], argv[2], 5));
-		//printf("%s", strnstr(argv[1], argv[2], 5));
-	}
 }
