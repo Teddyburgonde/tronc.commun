5bd6211ca97686a09933ddc2eca72f2200fc7050
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-24 09:38:43 +0200

    add proto

--- ft.h
+++ ft.h
@@ -12,6 +12,8 @@ int						ft_memcmp(const void *s1, const void *s2, size_t n);
 void					ft_bzero(void *s, size_t n);
 char					*ft_strchr(const char *s, int c);
 char					*ft_strrchr(const char *s, int c);
+char					*ft_strnstr(const char *haystack,
+							const char *needle, size_t len);
 int						ft_strncmp(const char *s1, const char *s2, size_t n);
 int						ft_isalpha(int c);
 int						ft_isdigit(int c);
