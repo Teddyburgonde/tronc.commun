88c90be33cf39dbe6c043915a2f9236749327173
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-24 09:30:41 +0200

    -mfinish function ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*tab1;
+	const unsigned char	*tab2;
+	size_t				i;
+
+	i = 0;
+	tab1 = s1;
+	tab2 = s2;
+	while ((tab1[i] || tab2[i]) && (i < n))
+	{
+		if (tab1[i] != tab2[i])
+			return (tab1[i] - tab2[i]);
+		i++;
+	}
+	return (0);
+}
