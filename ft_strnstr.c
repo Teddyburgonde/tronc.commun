4710d74e0697b344d5f58e2775dcbf5a20478bee
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-25 09:19:56 +0200

    finish function ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,22 +1,22 @@
 #include <stddef.h>
 
-char	*ft_strnstr(const char *str, const char *to_find, int len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int	i;
-	int	j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
-	if (to_find[0] == '\0')
-		return ((char *)str);
-	while (str[i])
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i])
 	{
 		j = 0;
-		while (str[i + j] == to_find[j])
+		while (haystack[i + j] == needle[j])
 		{
 			if (i >= len)
 				break ;
-			if (to_find[j + 1] == '\0')
-				return ((char *)&str[i]);
+			if (needle[j + 1] == '\0')
+				return ((char *)&haystack[i]);
 			j++;
 		}
 		i++;
