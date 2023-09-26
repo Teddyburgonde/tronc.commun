8af7a48edd49f987c5d9e673020fa8334c0d8ea7
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-26 13:06:55 +0200

    finish

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,7 +1,6 @@
-#include <stddef.h>
-#include <stdio.h>
+#include <stdlib.h>
 
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t		i;
 	char		*ptr_dest;
