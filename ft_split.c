06b241fe03a0a9f6258b393b449fa08fa5573ec8
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-28 15:25:21 +0200

    ft_separator ok and wd_count_words ok

--- ft_split.c
+++ ft_split.c
@@ -14,8 +14,32 @@ int	ft_separator(char c, const char *str)
 	return (0);
 }
 
-
-char **ft_split(char const *s, char c)
+int	wd_count_words(char *str, char c)
 {
+	int	i;
+	int	count;
 
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] && ft_separator(str[i], str))
+			i++;
+		if (str[i] && !ft_separator(str[i], str))
+		{
+			while (str[i] && !ft_separator(str[i], str))
+			{
+				i++;
+			}
+			count++;
+		}
+	}
+	return (count);
+}
+
+char **ft_split(const char *s, char c)
+{
+	char	**tab;
+
+	
 }
