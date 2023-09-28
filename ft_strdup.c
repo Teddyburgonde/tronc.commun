39c9c7c8aa134e4560c96bfee7e7c92ce5ecfaf2
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-28 14:30:01 +0200

    add verif ok allocation memory

--- ft_strdup.c
+++ ft_strdup.c
@@ -17,11 +17,14 @@ char	*ft_strdup(const char *s1)
 
 	i = 0;
 	tab = malloc(sizeof(char) * ft_strlen(s1) + 1);
-	while (s1[i])
+	if (tab)
 	{
-		tab[i] = s1[i];
-		i++;
+		while (s1[i])
+		{
+			tab[i] = s1[i];
+			i++;
+		}
+		tab[i] = '\0';
 	}
-	tab[i] = '\0';
 	return ((char *)tab);
 }
