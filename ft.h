ff3aa887ae7a5082ec707a0fcc85c08ddb863e1d
Teddyburgonde <bandama.teddy@gmail.com>
2023-09-28 15:13:24 +0200

    bug fixed

--- ft.h
+++ ft.h
@@ -27,7 +27,7 @@ void					*calloc(size_t count, size_t size);
 char					*ft_strdup(const char *s1);
 char					*ft_substr(const char *s,
 							unsigned int start, size_t len);
-char					*ft_strjoin(char const *s1, char const *s2);
-char					*ft_strtrim(char const *s1, char const *set);
-char					**ft_split(char const *s, char c);
+char					*ft_strjoin(const char *s1, const char *s2);
+char					*ft_strtrim(const char *s1, const char *set);
+char					**ft_split(const char *s, char c);
 #endif
