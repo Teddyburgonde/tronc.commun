9b0ccd9f03e2e89d64f3427cd70abdbd6432ac22
Teddyburgonde <bandama.teddy@gmail.com>
2023-10-02 16:26:03 +0200

    begin

---
+++ ft_itoa.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// convert interger to string
+
+char	*ft_itoa(int n)
+{
+	if ( n >= '0' && n <= '9')
+	{
+				
+	}
+}
+
+
+int	main(void)
+{
+	int		c;
+	char	buf[20];
+
+	c = 15;
+	sprintf(buf, "%d", c);
+	printf("%s", buf);
+}
