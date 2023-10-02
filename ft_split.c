2714497cf109a5b58a2debdd6fdb6c82f77051b8
Teddyburgonde <bandama.teddy@gmail.com>
2023-10-02 09:43:48 +0200

    ok but wrong version

--- ft_split.c
+++ ft_split.c
@@ -37,28 +37,62 @@ int	wd_count_words(char *str, char *charset)
 	return (count);
 }
 
-char	*ft_strdup(const char *str, char *charset)
+char	*ft_strdup(char *str, char *charset)
 {
-	char	*tab;
 	int		i;
+	char	*word;
 
 	i = 0;
 	while (str[i] && !ft_separator(str[i], charset))
 		i++;
-	tab = malloc(sizeof(char) * (i + 1));
+	word = malloc(sizeof(char) * (i + 1));
 	i = 0;
 	while (str[i] && !ft_separator(str[i], charset))
 	{
-		tab[i] = str[i];
+		word[i] = str[i];
 		i++;
 	}
-	tab[i] = '\0';
-	return (tab);
+	word[i] = '\0';
+	return (word);
 }
 
-char **ft_split(const char *s, char c)
+char	**ft_split(char *str, char *charset)
 {
 	char	**tab;
+	int		i;
+	int		j;
 
-	
+	i = 0;
+	j = 0;
+	tab = malloc(sizeof(char *) * (wd_count_words(str, charset) + 1));
+	while (str[i])
+	{
+		while (str[i] && ft_separator(str[i], charset))
+			++i;
+		if (str[i] && !ft_separator(str[i], charset))
+		{
+			tab[j] = ft_strdup(&(str[i]), charset);
+			j++;
+			while (str[i] && !ft_separator(str[i], charset))
+				++i;
+		}
+	}
+	tab[j] = 0;
+	return (tab);
+}
+#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	int	i;
+	char	**tab;
+
+	(void)argc;
+	i = 0;
+	tab = ft_split(argv[1], argv[2]);
+	while (tab[i])
+	{
+		printf("|%s| ", tab[i]);
+		i++;
+	}
+	free(tab);
 }
