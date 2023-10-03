5ad39460d003061798bc5dcfb89c12b8dac39998
Teddyburgonde <bandama.teddy@gmail.com>
2023-10-03 16:50:35 +0200

    finish function ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
 
-// convert interger to string
-
 int	ft_number_len(int n)
 {
 	int	len;
@@ -15,26 +13,55 @@ int	ft_number_len(int n)
 	return (len);
 }
 
+int	cut(int n, int pos)
+{
+	int	i;
+
+	i = 0;
+	while (i < pos - 1)
+	{
+		n /= 10;
+		i++;
+	}
+	n %= 10;
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
+
+void	initialize_value(int *sign, int *i)
+{
+	*sign = 0;
+	*i = 0;
+}
+
 char	*ft_itoa(int n)
 {
 	char	*tab;
-	int		sign;
 	int		len;
+	int		sign;
+	int		i;
 
-	sign = 0;
+	initialize_value(&sign, &i);
 	len = ft_number_len(n);
 	if (n < 0)
 	{
 		sign = 1;
+		i = 1;
+		len++;
 	}
+	tab = malloc(sizeof(char) * (len + 1));
+	if (!tab)
+		return (NULL);
 	if (sign == 1)
-	{
 		tab[0] = '-';
+	while (i < len)
+	{
+		tab[i] = cut(n, len - i) + '0';
+		i++;
 	}
+	tab[i] = '\0';
 	return (tab);
 }
-
-
-#include <stdio.h>
-
-
