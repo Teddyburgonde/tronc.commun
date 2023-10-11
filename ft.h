eeda5a010c207ed109fd131cac4a5ea2e6e5b899
Teddyburgonde <bandama.teddy@gmail.com>
2023-10-11 07:32:12 +0200

    add proto ft_putchar_fd

--- ft.h
+++ ft.h
@@ -34,4 +34,5 @@ char					*ft_itoa(int n);
 char					*ft_strmapi(const char *s,
 							char (*f)(unsigned int, char));
 void					ft_striteri(char *s, void (*f)(unsigned int, char*));
+void					ft_putchar_fd(char c, int fd);
 #endif
