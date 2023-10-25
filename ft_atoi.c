8b3bb50200e89f3fe1b129abfdd8d9d0f1bbb399
Teddyburgonde <bandama.teddy@gmail.com>
2023-10-25 17:31:34 +0200

    modif fix bug not ok

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,24 +1,50 @@
+#include "libft.h"
+
+int	is_whitespace(char c)
+{
+	return (c == ' ' || (c >= '\t' && c <= '\r'));
+}
+
+void	ft_init(int *i, int *a, int *sign)
+{
+	*i = 0;
+	*a = 0;
+	*sign = 1;
+}
+
+void	script_sign(const char *str, int *i, int *sign)
+{
+	while ((str[*i] == '+' || str[*i] == '-') || *sign == 0)
+	{
+		if (str[*i] == '-')
+			*sign = -1;
+		(*i)++;
+	}
+}
+
 int	ft_atoi(const char *str)
 {
 	int	i;
 	int	a;
 	int	sign;
 
-	i = 0;
-	a = 0;
-	sign = 1;
-	while (str[i] == 32)
+	ft_init(&i, &a, &sign);
+	while (is_whitespace(str[i]))
 		i++;
-	while ((str[i] == '+' || str[i] == '-') && sign == 0)
-	{
-		if (str[i] == '-')
-		{
-			sign = -1;
-		}
-		i++;
-	}
+	script_sign(str, &i, &sign);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
+		if (sign == -1)
+		{
+			if (a < (-2147483648 - (str[i] - '0')) / 10)
+				return (0);
+		}
+		else if (a > (2147483647 - (str[i] - '0')) / 10)
+		{
+			if (a > (4294967295 - (str[i] - '0')) / 10)
+				return (0);
+			return (-1);
+		}
 		a = a * 10 + (str[i] - '0');
 		i++;
 	}
